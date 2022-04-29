7225a0836a11993ea3fa85247cfc194f4c38e7c5
adamkumlin <kumlinadam99@gmail.com>
2022-04-29 22:42:28 +0200

    v

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,24 +1,25 @@
 <?php
-    
-    if (isset($_POST["contactSubmit"])) {
-        
         $user_name = $_POST["contactName"];
         $user_email = $_POST["contactEmail"];
         $user_message = $_POST["contactMessage"];
-    
-        if (IsValid($user_email)) {
-            echo "Fel med e-postadressen!";
-            exit;
-        }
-    
-        $email_from = "ehnsbbq@gmail.com";
-        $email_subject = "Nytt meddelande";
-        $email_body = "Du har fått ett nytt meddelande från $user_name.\n". "Här är meddelandet:\n $user_message".
-    
+        // Hämtar namnet, e-postadressen och meddelandet från formuläret och tilldelar dem tre olika variabler.
+
+        $from = "info@ehnsbbq.se";
         $to = "ehnsbbq@gmail.com";
-        $headers = "From: $email_from \r\n";
-    
-        mail($to,$email_subject,$email_body,$headers);
-        echo "mail sent";
-    }
+        // Specificerar från vilken e-postadress mejlet ska skickas och vilken e-postadress mejlet ska skickas till. Informationen sparas i två olika variabler.
+
+        $subject = "Nytt meddelande från en användare på ehnsbbq.se";
+        // Specificerar ämnet som kommer synas i mejlet.
+
+        $headers = "From: $from\n";
+        // Headers (en valfri parameter) ges variabeln "$from":s värde.
+
+        $message = "E-postadress: $user_email\n
+        Namn: $user_name\n
+        Meddelande: $user_message";
+        // Skapar meddelandet som faktiskt mejlet ska innehålla. I meddelandet skrivs användarens mejladress, namn och meddelande ut.
+
+        mail($to,$subject,$message,$headers);
+        // Slutligen ska mejlet skickas, detta görs med funktionen mail(). Här specificeras vilken e-postadress mejlet ska skickas till ($to), mejlets ämmne ($subject),
+        // meddelandet (det som mejlet ska innehålla, $message) och $headers (avsändaren ($from)).
 ?>
