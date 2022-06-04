11a716e0fb549e0068e1ca53f882aa6600f56510
adamkumlin <kumlinadam99@gmail.com>
2022-06-04 18:31:17 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const toggleHamburgerMenu = document.getElementsByClassName("navToggle")[0];
 const navLinks = document.getElementsByClassName("navLinks")[0];
-// Skapar två variabler som kopplas till sina respektive element. De första elementen med klasserna i varje HTML-fil hittas.
+// Skapar två konstanter som kopplas till sina respektive element. De första elementen med klasserna "navToggle" och "navLinks" i varje HTML-fil hittas.
 
 toggleHamburgerMenu.addEventListener("click", () => {
 // När elementet klickas på.
