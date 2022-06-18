e4180dde6e67d305d56716a8224f4f88cee8a90e
adamkumlin <kumlinadam99@gmail.com>
2022-06-18 19:01:33 +0200

    Revert "Update contact.php"
    This reverts commit fae33b539e130eed66272108ef65da569a2a70ea.

--- contact.php
+++ contact.php
@@ -219,9 +219,6 @@
                 </p>
                 <!--Skriver en text med länkar och bilder inbäddade.-->
 
-                <p>Genom att kontakta oss via mejl eller via e-post så accepterar du vår <a href="privacy.html">integritetspolicy<a>.</p>
-                <!--Skriver en notis om integritetspolicyn.-->
-
                 <p>Du får också gärna använda formuläret nedan, vi ser fram emot ditt meddelande!</p>
             </div>
 
